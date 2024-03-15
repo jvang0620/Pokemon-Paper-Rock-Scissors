822da6ff8639252e4447595ef1abd9627d7cadcb
Jai_Vang <jaivang0387@gmail.com>
2024-03-14 20:48:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Pokemon Rock Paper Scissors
+
+<div align="center">
+    <img src="/images/app_image_2.JPG" alt="Screenshot of Live Server Extension" width="600">
+</div>
+
+### Overview:
+
+In 'Pokemon Rock Paper Scissors,' players engage in a strategic battle reminiscent of the classic game with a thrilling twist inspired by the iconic Pokemon universe. Choose your Pokemon avatar from the selections. Bulbasaur, Geodude, and Scyther will represent a different element - Paper, Rock, or Scissors. Pit your chosen Pokemon against your opponent's (Team Rocket) in an intense showdown where strategy and quick thinking are key.
+
+Will you emerge victorious, harnessing the strengths of your chosen Pokemon, or will you fall to the cunning tactics of your rival? Test your skills against the computer in this exciting adaptation of a beloved childhood game. First to win 5 matches is the winner.
+
+Get ready to embark on a journey filled with excitement, surprises, and the thrill of Pokemon battles in 'Pokemon Rock Paper Scissors'! May the best Pokemon trainer prevail!
+
+### Components & Features
+
+`Pokemon Avatar Selection`: Allow players to choose their avatar from a selection of Pokemon characters such as Bulbasaur, Geodude, and Scyther, each representing a different element (Paper, Rock, or Scissors).
+
+`Computer Opponent (Team Rocket)`: Implement an AI opponent that selects its Pokemon avatar randomly or based on a predefined strategy to compete against the player.
+
+`Game Rounds`: Structure the game into multiple rounds, where each round consists of the player selecting their Pokemon and the computer opponent doing the same.
+
+`Game Logic`: Implement the game's logic to determine the winner of each round based on the classic rules of Rock Paper Scissors: Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.
+
+`Score Tracking`: Keep track of the player's and computer's scores throughout the game, updating them after each round based on the outcome.
+
+`Victory Condition`: Set a condition for winning the game, such as being the first player to reach a certain number of wins (e.g., 5 wins).
+
+`UI Components`: Design and implement user interface components to display the game elements, including player and computer avatars, scoreboards, round information, and game instructions.
+
+`Animations and Visual Effects`: Enhance the user experience with animations and visual effects to make the game more engaging and immersive.
+
+<!-- Audio Effects: Add sound effects and background music to create a dynamic and immersive gaming atmosphere.
+
+Responsive Design: Ensure that the app is responsive and accessible across various devices and screen sizes, providing a seamless gaming experience for players on desktop and mobile devices. -->
+
+`Game Controls`: Implement intuitive controls for players to interact with the game, such as clicking or tapping on Pokemon avatars to make selections.
+
+`Game State Management`: Manage the state of the game, including initializing the game, starting new rounds, updating scores, and determining the winner.
+
+#### Getting Started:
+
+1. Clone the repository to your local machine one of the following:
+
+   HTTPS: <pre><code>git clone https://github.com/jvang0620/Pokemon-Paper-Rock-Scissors</code></pre>
+
+2. Add extension "Live Server" if you havn't:
+
+<div align="center">
+    <img src="/images/Live-Server-Pic.PNG" alt="Screenshot of Live Server Extension" width="600">
+</div>
+
+3. Next, right-click anywhere in the HTML file and select "Open with Live Server", and the app will display on your local machine.
+
+You're all set. Happy coding!!!:)
+
+### Developed Using:
+
+![HTML](https://img.shields.io/badge/-HTML-orange?style=for-the-badge&logo=html5)
+![CSS](https://img.shields.io/badge/-CSS-blue?style=for-the-badge&logo=css3)
+![JavaScript](https://img.shields.io/badge/-JavaScript-yellow?style=for-the-badge&logo=javascript)
---
+++ script.js
@@ -0,0 +1,161 @@
+/*
+    getComputerChoice() Function that returns a random string between rock paper scissors
+    @Return String Computer's weapon choice
+*/
+function getComputerChoice() {
+    const rand = Math.floor(Math.random() * 3); //to hold either 0, 1, or 2
+    return rand; //0 is rock, 1 is paper, 2 is scissors
+}
+
+/*
+    playRound() Function that starts a round of the game and returns the winner
+    @Return String To let user know if they won the round or not
+*/
+function playRound(player, computer) {
+
+    //if both have the same weapon, then tie
+    if (player === computer) {
+        return "It's a Tie!";
+    }
+    //if user has rock...
+    else if (player === 0) {
+        //...and computer has paper, then user loses
+        if (computer === 1)
+            return "You Lose! Bulbasaur (paper) beats Geodude (rock)!";
+        //...and computer has scissors, then user wins
+        else
+            return "You Win! Geodude (rock) beats Scyther (scissors)!";
+    }
+    //if user has paper...
+    else if (player === 1) {
+        //...and computer has rock, then user wins
+        if (computer === 0)
+            return "You Win! Bulbasaur (paper) beats Geodude (rock)!";
+        //...and computer has scissors, then user loses
+        else
+            return "You Lose! Scyther (scissors) beat Bulbasaur (paper)!";
+    }
+    //if user has scissors...
+    else if (player === 2) {
+        //...and computer has paper, user wins
+        if (computer === 1)
+            return "You Win! Scyther (scissors) beat Bulbasaur (paper)!";
+        //...and computer has rock, user loses
+        else
+            return "You Lose! Geodude (rock) beats Scyther (scissors)!";
+    }
+    //if user has wrong input, then error
+    else
+        return "Error: Player did not choose weapon correctly."
+}
+
+/*
+    game() Function that starts a new game and returns the winner of the entire game
+    @Return String To let user know if they won the game or not
+*/
+function game(player) {
+
+    if (playerPoints === 5 || computerPoints === 5) {
+        return;
+    }
+
+    rounds++;
+    roundNum.textContent += "\nRound " + rounds;
+    //loop 5 times for 5 rounds per game
+    let computer = getComputerChoice(); //get computer input
+
+    if (player === 0) {
+        const para = document.createElement("p");
+        para.textContent = "Rock";
+        pChoice.appendChild(para);
+    } else if (player === 1) {
+        const para = document.createElement("p");
+        para.textContent = "Paper";
+        pChoice.appendChild(para);
+    } else {
+        const para = document.createElement("p");
+        para.textContent = "Scissors";
+        pChoice.appendChild(para);
+    }
+
+    if (computer === 0) {
+        const para = document.createElement("p");
+        para.textContent = "Rock";
+        cChoice.appendChild(para);
+    } else if (computer === 1) {
+        const para = document.createElement("p");
+        para.textContent = "Paper";
+        cChoice.appendChild(para);
+    } else {
+        const para = document.createElement("p");
+        para.textContent = "Scissors";
+        cChoice.appendChild(para);
+    }
+
+    let s = playRound(player, computer); //start round
+
+    //console.log(s); //display winner of round
+    roundNum.textContent = s;
+    //if player wins, then add point
+    if (s.includes('Win'))
+        playerPoints++;
+    //if computer wins, then add point
+    else if (s.includes('Lose'))
+        computerPoints++;
+    //if tie, add point to both
+    else if (s.includes('Tie')) {
+    }
+    //if error, then error
+    else
+        return;
+
+    score.textContent = playerPoints + " - " + computerPoints;
+
+
+    if (playerPoints === 5 || computerPoints === 5) {
+        roundNum.textContent = checkWinner();
+    }
+
+}
+
+//start game
+//console.log(game());
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const pChoice = document.querySelector('.player-log');
+const cChoice = document.querySelector('.computer-log');
+const score = document.querySelector('.score');
+const roundNum = document.querySelector('.round-number');
+
+
+let rounds = 0;
+let playerPoints = 0; //to hold player points
+let computerPoints = 0; //to hold computer points
+
+
+rock.addEventListener('click', function () {
+    game(0);
+});
+
+paper.addEventListener('click', function () {
+    game(1);
+});
+
+scissors.addEventListener('click', function () {
+    game(2);
+});
+
+function checkWinner() {
+    //if player points is greater than computer points, then player wins
+    if (playerPoints > computerPoints) {
+        return "You beat Team Rocket! You are the Winner!!! :D";
+    }
+    //if computer points is greater than player points, then computer wins
+    else if (playerPoints < computerPoints) {
+        return "Team Rocket Wins! Better Luck Next Time! :(";
+    }
+    //else, it is a tie (should not be possible)
+    else
+        return "ERROR: It's a tie?"
+}
